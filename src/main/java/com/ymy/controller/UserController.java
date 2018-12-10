dd8ee8fd642289e3f09f59f845957d13c93614f6
安能 <271066981@qq.com>
2018-12-10 22:20:13 +0800

    2018年12月10日22:20:06

--- src/main/java/com/ymy/controller/UserController.java
+++ src/main/java/com/ymy/controller/UserController.java
@@ -2,7 +2,6 @@ package com.ymy.controller;
 
 import com.ymy.entity.User;
 import com.ymy.mapper.UserMapper;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -17,13 +16,19 @@ public class UserController {
     @Resource
     UserMapper userMapper;
 
+
+    @RequestMapping(value = "/index", method = RequestMethod.GET)
+    public String index() {
+        return "index";
+    }
+
     /**
-     *RequestMapping value 代表访问路径，method:访问方式
+     * RequestMapping value 代表访问路径，method:访问方式
      * ResponseBody 把他相应给浏览器
-     * */
-    @RequestMapping(value = "/finduser",method = RequestMethod.GET)
+     */
+    @RequestMapping(value = "/finduser", method = RequestMethod.GET)
     @ResponseBody
-    public User findUser(){
+    public User findUser() {
         User user = userMapper.selectById(1);
      /*   List<User> user = userMapper.findUser();
         System.out.println(user);*/
---
+++ src/main/java/com/ymy/util/a.java
@@ -0,0 +1,4 @@
+package com.ymy.util;
+
+public class a {
+}
